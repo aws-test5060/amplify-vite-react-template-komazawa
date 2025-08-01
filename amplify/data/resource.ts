2e41d3b82987f5959d7b91a966daf03fcb6e4b30
aws-test5060 <aws-github-test5060@outlook.com>
2025-08-01 18:48:39 +0900

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,24 +7,21 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+ Todo: a
+   .model({
+     content: a.string(),
+   })
+   .authorization((allow) => [allow.authenticated()]), // 変更
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
+ schema,
+ authorizationModes: {
+   defaultAuthorizationMode: "userPool", // 変更
+   // API 認証の記述を削除
+ },
 });
 
 /*== STEP 2 ===============================================================
